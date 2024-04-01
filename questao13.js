c1e19519bc996a74e7e34c73cfb1f9133df3b897
camilyalves <camily10.alves2007@gmail.com>
2024-04-01 17:01:53 -0400

    finalizado questao 13

---
+++ questao13.js
@@ -0,0 +1,10 @@
+/*
+Estrutura de repetição do...while: 
+Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente. 
+*/
+let i = 10;
+do {
+    console.log(i);
+    i --;
+} while (i >= 1);
