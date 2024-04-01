c877a4fa212a5a71d475ea26ba0e5020c967672b
camilyalves <camily10.alves2007@gmail.com>
2024-04-01 17:02:03 -0400

    finalizado questao 14

---
+++ questao14.js
@@ -0,0 +1,10 @@
+/*
+Estrutura de repetição do...while: 
+Questão: Escreva um código que imprima os números de 0 a 100 em 
+incrementos de 10. 
+*/
+let i = 0;
+do {
+    console.log(i);
+    i += 10;
+} while (i <= 100);
