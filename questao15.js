b4b2c515dd878d76ad0b202a87f7697e221ba956
camilyalves <camily10.alves2007@gmail.com>
2024-04-01 17:02:12 -0400

    finalizado questao 15

---
+++ questao15.js
@@ -0,0 +1,10 @@
+/*
+Estrutura de repetição do...while: 
+Questão: Escreva um código que imprima a tabuada do 5 (de 1 a 10). 
+*/
+let i = 1;
+do {
+    let resultado = 5 * i;
+    console.log("5 x " + i + " = " + resultado);
+    i++;
+} while (i <= 10);
