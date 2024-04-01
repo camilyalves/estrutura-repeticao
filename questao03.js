9d5f7e7c43a85e3b37daecd401f0b158874fc702
camilyalves <camily10.alves2007@gmail.com>
2024-04-01 16:59:54 -0400

    finalizado questao 03

---
+++ questao03.js
@@ -0,0 +1,8 @@
+/*
+Estrutura de repetição for:
+Questão: Escreva um código que imprima os números de 10 a 1 em 
+ordem decrescente.  
+*/
+for (let i = 10; i >= 1; i -= 1 ) {
+    console.log("contagem: " + i)
+} 
