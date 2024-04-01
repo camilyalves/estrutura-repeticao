5551223f9fa8ae8025aeab9a0d729d6fad6d5190
camilyalves <camily10.alves2007@gmail.com>
2024-04-01 17:01:43 -0400

    finalizado questao 12

---
+++ questao12.js
@@ -0,0 +1,9 @@
+/*
+Estrutura de repetição do...while: 
+Questão: Escreva um código que imprima os números pares de 2 a 10 
+*/
+let i = 2;
+do {
+    console.log(i);
+    i += 2;
+} while (i <= 10);
