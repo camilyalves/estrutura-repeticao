1c568affe6bdb95b91510ad17a179fc8a6903ea7
camilyalves <camily10.alves2007@gmail.com>
2024-04-01 17:00:23 -0400

    finalizado questao 05

---
+++ questao05.js
@@ -0,0 +1,8 @@
+ /*
+Estrutura de repetição for:
+Questão: Escreva um código que imprima a tabuada do 5 (de 1 a 10)
+*/
+for (let i = 1; i <= 10; i++) {
+    let resultado = 5 * i;
+    console.log("5 x " + i + " = " + resultado);
+}
